72413d6c77b8f3598b3e561f1b96d743a11d222a
Joshua Stein <josh.stein@flexshopper.com>
2016-05-18 13:28:14 -0400

    try overrides

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-var winston = require('winston')
+'use strict';
+
+var util = require('util'),
+    winston = require('winston');
+
+module.exports = {
+    middleware: function(req, res, next){
+        console.info(req.method, req.url, res.statusCode);
+        next();
+    }
+};
 
 if (process.env.DEIS_LOGGING_ENABLED) {
   logging_options = {
@@ -22,3 +32,27 @@ if (process.env.DEIS_LOGGING_ENABLED) {
     exitOnError: false
   }
 }
+var logger = new winston.Logger( logging_options );
+
+// Override the built-in console methods with winston hooks
+function formatArgs(args){
+    return [util.format.apply(util.format, Array.prototype.slice.call(args))];
+}
+
+console.log = function(){
+    logger.info.apply(logger, formatArgs(arguments));
+};
+console.info = function(){
+    logger.info.apply(logger, formatArgs(arguments));
+};
+console.warn = function(){
+    logger.warn.apply(logger, formatArgs(arguments));
+};
+console.error = function(){
+    logger.error.apply(logger, formatArgs(arguments));
+};
+console.debug = function(){
+    logger.debug.apply(logger, formatArgs(arguments));
+};
+
+
